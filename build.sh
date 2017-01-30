6e6eec36de1cf26f40c09f08ed819eb42d9b8ee7
Stan Grishin <stangri@melmac.net>
2017-01-30 00:20:06 -0800

    170129 build

--- build.sh
+++ build.sh
@@ -31,11 +31,14 @@ function editfiles() {
 		sed  -i '' "s/showthread.php?t=3432382/showthread.php?t=3521901/" MagiskManager/app/src/main/java/com/topjohnwu/magisk/AboutActivity.java)
 }
 
-[ "$1" = "setup" ] && setup && exit 0
+start=$(date +%s.%N)
 
-if [ "$1" = "sign" ]; then
-	signapp
-else
+case $1 in
+	setup)
+		setup;;
+	sign)
+		signapp;;
+	   *)
 	if ! git -C Magisk ${CMP} || [ -n "$1" ]; then
 		[ -z "$1" ] && { echo "Magisk:		new commits found!"; git -C Magisk pull --recurse-submodules; }
 		echo -e -n "Building Magisk-v${MAGISKVER}-${suffix}.zip...		"
@@ -62,11 +65,14 @@ else
 	else
 		echo "MagiskManager:	no new commits!"
 	fi
-fi
+	[ ! -d MagiskFiles ] && mkdir -p MagiskFiles
+	mv -f Magisk*.zip MagiskFiles/ >/dev/null 2>&1
+	mv -f Magisk*.apk MagiskFiles/ >/dev/null 2>&1
+	if [ -n "$updates" ]; then
+		echo -e -n "Pushing new files to github.com/stangri...	" && git -C MagiskFiles add . && git -C MagiskFiles commit -m "$suffix build" >/dev/null 2>&1 && git -C MagiskFiles push origin >/dev/null 2>&1 && echo "Done!" || echo "FAIL!"
+	fi
+	;;
+esac
 
-[ ! -d MagiskFiles ] && mkdir -p MagiskFiles
-mv -f Magisk*.zip MagiskFiles/ >/dev/null 2>&1
-mv -f Magisk*.apk MagiskFiles/ >/dev/null 2>&1
-if [ -n "$updates" ]; then
-	echo -e -n "Pushing new files to github.com/stangri...	" && git -C MagiskFiles add . && git -C MagiskFiles commit -m "$suffix build" >/dev/null 2>&1 && git -C MagiskFiles push >/dev/null 2>&1 && echo "Done!" || echo "FAIL!"
-fi
+end=`date +%s.%N`; runtime=$(echo "$end - $start" | bc -l); secs=$(printf %.f $runtime);
+echo -e -n "Total running time: $(printf '%02dh:%02dm:%02ds\n\n' $(($secs/3600)) $(($secs%3600/60)) $(($secs%60)))"
