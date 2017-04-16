f544e9686a71d2a2a7a6d042e5e026fcdf3a5d4a
Stan Grishin <stangri@melmac.net>
2017-04-16 09:14:53 -0700

    170416 build

--- build.sh
+++ build.sh
@@ -143,13 +143,12 @@ if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
 				fi
 				echo -e -n "Editing  Magisk files...			" && git -C Magisk checkout master >/dev/null 2>&1 && edit_magisk_files && ok || fail
 				echo -e -n "Building Magisk-v${MAGISKVER}-${suffix}.zip...		"
-				(cd Magisk; git submodule update --recursive >/dev/null 2>&1; ./build.sh all ${suffix} >/dev/null 2>&1;)
+				(cd Magisk; git pull --recurse-submodules >/dev/null 2>&1; git submodule update --recursive >/dev/null 2>&1; ./build.sh all ${suffix} >/dev/null 2>&1;)
 				[ -f Magisk/Magisk-v${suffix}.zip ] && { ok; mv Magisk/Magisk-v${suffix}.zip Magisk-v${MAGISKVER}-${suffix}.zip; } || fail
 				git -C Magisk reset --hard HEAD >/dev/null 2>&1
-				updates=1
 				echo -e -n "Editing  Magisk (UD) files...			" && git -C Magisk checkout unify_daemon >/dev/null 2>&1 && edit_magisk_files && ok || fail
 				echo -e -n "Building Magisk-UD-v${MAGISKVER}-${suffix}.zip...		"
-				(cd Magisk; git submodule update --recursive >/dev/null 2>&1; ./build.sh all ${suffix} >/dev/null 2>&1;)
+				(cd Magisk; git pull --recurse-submodules >/dev/null 2>&1; git submodule update --recursive >/dev/null 2>&1; ./build.sh all ${suffix} >/dev/null 2>&1;)
 				[ -f Magisk/Magisk-v${suffix}.zip ] && { ok; mv Magisk/Magisk-v${suffix}.zip Magisk-UD-v${MAGISKVER}-${suffix}.zip; } || fail
 				git -C Magisk reset --hard HEAD >/dev/null 2>&1
 				git -C Magisk checkout master >/dev/null 2>&1
